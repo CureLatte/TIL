18222ba941b5a5b1e2ad7eda576a870cc0b1ab19
zhqmfkv <zhqmfkv@naver.com>
2022-05-13 20:37:13 +0900

    5월 13일자 알고리즘2문제 완

--- Algorithm/Programmers/Level2/전화번호목록.java
+++ Algorithm/Programmers/Level2/전화번호목록.java
@@ -7,21 +7,35 @@ class Solution {
         boolean answer = true;
         Arrays.sort(phone_book);
 
-        int current_len = phone_book[phone_book.length-1].length();
-        String target = phone_book[phone_book.length-1];
-
+        int current_len = phone_book[0].length();
+        String target = phone_book[0];
+        String prevalue = "";
         for(int i=0; i<phone_book.length; i++){
-            if(current_len >= phone_book[i].length()){
+            // 중복확인
+            if (prevalue.compareTo(phone_book[i])==0){
+                return false;
+            } else {
+                prevalue = phone_book[i];
+            }
+
+            // 길이가 바뀌였을 때
+            // 수정사항 -> TC 가 ["934793", "34", "44", "9347"] -> False
+            // 길이 기준 + 여전히 앞자리인지 확인
+            if(current_len >= phone_book[i].length() || target.compareTo(phone_book[i].substring(0, current_len)) !=0){
+
                 target = phone_book[i];
                 current_len = phone_book[i].length();
             }
             else{
-                if(target.compareTo(phone_book[i].substring(0, current_len)) == 0 ){
+                String check = phone_book[i].substring(0, current_len);
+                if(target.compareTo(check) == 0 ){
                     return false;
                 }
+
             }
 
         }
+
         return true;
     }
 }
