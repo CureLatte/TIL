cf19f289ad6ea96273ed464859fa814539abf63f
zhqmfkv <zhqmfkv@naver.com>
2022-06-05 01:13:47 +0900

    6월 4일자 TIL - 알고리즘 추가

--- Algorithm/2.DFS/exercise.py
+++ Algorithm/2.DFS/exercise.py
@@ -1,44 +1,27 @@
-# 이진탐색 연습장
-# 외우기 코드!
-
 import sys
-from collections import deque
+input = sys.stdin.readline
+m = int(input())
+n = int(input())
+
+graph = [[0] * (m+1) for _ in range(m+1) ]
+
+for _ in range(n):
+    n1, n2 = map(int, input().split())
+    graph[n1][n2] = 1
+    graph[n2][n1] = 1
+
+visited = []
 
 
-def search(nums, st, en, target):
-    # 함수 종료 조건
-    if st == en:
-        # 맞 았을때
-        if nums[st] == target:
-            print(1)
-        # 맞지 않았을 때
-        else:
-            print(0)
+def dfs(computer):
+    global visited
+    if computer in visited:
         return
-    mid = (st + en) // 2
-    if nums[mid] < target:
-        search(nums, mid + 1, en, target)
-    else:
-        search(nums, st, mid, target)
+    visited.append(computer)
+    for idx, i in enumerate(graph[computer]):
+        if i == 1:
+            dfs(idx)
 
 
-def solution(standard, check_list):
-    ordered = sorted(standard)
-    for check in check_list:
-        search(ordered, 0, len(ordered)-1, check)
-
-
-def main():
-    # sys.stdin = open('input.txt', 'r')
-
-    n = int(input())
-    standard = list(map(int, input().split()))
-
-    m = int(sys.stdin.readline())
-    check_list = list(map(int, input().split()))
-
-    solution(standard, check_list)
-
-
-if __name__ == '__main__':
-    main()
+dfs(1)
+print(len(visited) -1 )
--- Algorithm/3. 백트래킹/exercise.py
+++ Algorithm/3. 백트래킹/exercise.py
@@ -4,41 +4,29 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 
-def search(nums, st, en, target):
-    # 함수 종료 조건
-    if st == en:
-        # 맞 았을때
-        if nums[st] == target:
-            print(1)
-        # 맞지 않았을 때
-        else:
-            print(0)
-        return
-    mid = (st + en) // 2
-    if nums[mid] < target:
-        search(nums, mid + 1, en, target)
-    else:
-        search(nums, st, mid, target)
+n, m = map(int, input().split())
+
+table = list(range(1, n+1))
+
+temp = []
 
 
-def solution(standard, check_list):
-    ordered = sorted(standard)
-    for check in check_list:
-        search(ordered, 0, len(ordered)-1, check)
+def search(deep, result, t):
+    if deep == m:
+        temp.append(result)
+        return result
+    v = []
+    for i in t:
+        v.append(i)
+        search(deep + 1, result + str(i), sorted(list(set(t) - set(v))))
 
 
-def main():
-    # sys.stdin = open('input.txt', 'r')
+search(0, '', table)
 
-    n = int(input())
-    standard = list(map(int, input().split()))
-
-    m = int(sys.stdin.readline())
-    check_list = list(map(int, input().split()))
-
-    solution(standard, check_list)
-
-
-if __name__ == '__main__':
-    main()
+for a in temp:
+    for _ in a:
+        print(int(_), end=' ')
+    print('')
+# print(len(temp))
