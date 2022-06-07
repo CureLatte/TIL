76f144c3be758f657d9fb511c85b37e0b16a96b5
zhqmfkv <zhqmfkv@naver.com>
2022-06-07 20:11:01 +0900

    6월 07일자 TIL - 생성

--- Algorithm/4.시뮬레이션/exercise.py
+++ Algorithm/4.시뮬레이션/exercise.py
@@ -4,36 +4,43 @@
 import sys
 from collections import deque
 
+input = sys.stdin.readline
 
-def solution(n, target, wait_list):
-    id_list = [(idx, priority) for idx, priority in enumerate(wait_list)]
+N, M = map(int, input().split())
 
-    printer = deque(id_list)
-    cnt = 0
-    while printer:
-        a = printer[0][1]
-        if max(printer, key=lambda x: x[1])[1] == a:
-            check = printer.popleft()
-            cnt += 1
-            if check[0] == target:
-                print(cnt)
-                break
-        else:
-            printer.rotate(-1)
+nr, nc, s_d = map(int, input().split())
+
+graph = []
+
+for a in range(N):
+    graph.append(list(map(int, input().split())))
 
 
-def main():
-    # sys.stdin = open('input.txt', 'r')
-
-    case_cnt = int(input())
-
-    all_case = []
-
-    for _ in range(case_cnt):
-        n, target = map(int, input().split())
-        primary_list = list(map(int, sys.stdin.readline().split()))
-        solution(n, target, primary_list)
+d_row = [-1, 0, 1, 0]
+d_col = [0, -1, 0, 1]
+cnt = 0
 
 
-if __name__ == '__main__':
-    main()
+def dfs(v, d):
+    global cnt
+    row, col = v
+    if graph[row][col] == 1:
+        return False
+    if graph[row][col] == 0:
+        cnt += 1
+        graph[row][col] = 2
+        for _ in range(4):
+            d = (d + 1) % 4
+            new_row = row + d_row[d]
+            new_col = col + d_col[d]
+            if 0 <= new_row <= N-1 and 0 <= new_col <= M-1:
+                dfs((new_row, new_col), d)
+
+    return True
+
+
+dfs((nr, nc), s_d)
+for a in graph:
+    print(a)
+
+print(cnt)
