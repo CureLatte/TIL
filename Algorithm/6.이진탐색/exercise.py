8bc43b5418180bef589b6bbc6cd161674fb18dc7
zhqmfkv <zhqmfkv@naver.com>
2022-06-10 23:56:13 +0900

    알고리즘 중간 저장

--- Algorithm/6.이진탐색/exercise.py
+++ Algorithm/6.이진탐색/exercise.py
@@ -9,4 +9,43 @@ N, M = map(int, input().split())
 
 tree = list(map(int, input().split()))
 
-print(tree)
+tree.sort(reverse=True)
+# print(tree)
+split_tree = 0
+
+
+def check(s, e, arrays):
+    global M, split_tree
+
+    mid = (s + e) // 2
+    # print(f'{s}, {mid}, {e}')
+
+    if s == e:
+        return s
+    cut_high = arrays[mid]
+    rest = 0
+    for tree in arrays:
+        if tree < cut_high:
+            break
+        rest += tree - cut_high
+    # print('rest: ', rest)
+    if rest < M:
+        return check(mid + 1, e, arrays)
+    elif rest > M:
+        return check(s, mid, arrays)
+    else:
+        return mid
+
+
+idx = check(0, len(tree)-1, tree)
+# print(idx)
+
+for cut in range(tree[idx], -1, -1):
+    split_tree = 0
+
+    for each_tree in tree[:idx+1]:
+        split_tree += each_tree - cut
+    # print(f'{cut}, {split_tree}')
+    if split_tree >= M:
+        print(cut)
+        break
