382b78710c71d9c6680fc074a74d39808fd48b17
zhqmfkv <zhqmfkv@naver.com>
2022-06-11 15:26:42 +0900

    6월 11일자 TIL - 생성

--- Algorithm/6.이진탐색/exercise.py
+++ Algorithm/6.이진탐색/exercise.py
@@ -12,40 +12,35 @@ tree = list(map(int, input().split()))
 tree.sort(reverse=True)
 # print(tree)
 split_tree = 0
+e = tree[0]
+s = 0
+answer = 0
+cut = 0
 
+while True:
 
-def check(s, e, arrays):
-    global M, split_tree
-
-    mid = (s + e) // 2
-    # print(f'{s}, {mid}, {e}')
-
-    if s == e:
-        return s
-    cut_high = arrays[mid]
-    rest = 0
-    for tree in arrays:
-        if tree < cut_high:
-            break
-        rest += tree - cut_high
-    # print('rest: ', rest)
-    if rest < M:
-        return check(mid + 1, e, arrays)
-    elif rest > M:
-        return check(s, mid, arrays)
-    else:
-        return mid
-
-
-idx = check(0, len(tree)-1, tree)
-# print(idx)
-
-for cut in range(tree[idx], -1, -1):
-    split_tree = 0
-
-    for each_tree in tree[:idx+1]:
-        split_tree += each_tree - cut
-    # print(f'{cut}, {split_tree}')
-    if split_tree >= M:
-        print(cut)
+    if s >= e:
+        if split_tree < M:
+            cut -= 1
         break
+    split_tree = 0
+    cut = (s + e) // 2
+    # print(s, e)
+    for each_tree in tree:
+        if each_tree <= cut:
+            break
+        split_tree += each_tree - cut
+    # print('split: ', split_tree, cut)
+
+    if split_tree < M:
+        e = cut
+        continue
+    elif split_tree == M:
+        break
+    else:
+        s = cut + 1
+        continue
+
+
+print(str(cut))
+
